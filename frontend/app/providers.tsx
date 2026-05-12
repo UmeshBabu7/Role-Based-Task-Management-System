837fec09204ce74fe9109bf01397bb6e8050bcc4
UmeshBabu7 <umeshtamang129@gmail.com>
2026-05-12 13:07:59 +0545

    feat: add Providers function

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,4 +1,19 @@
-"use client";
+'use client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
+import { MemoryRouter } from 'react-router';
+
+
 export function Providers({ children }: { children: React.ReactNode }) {
-  return { children };
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: { queries: { staleTime: 30 * 1000, retry: 1 } },
+  }));
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MemoryRouter>
+        {children}
+      </MemoryRouter>
+    </QueryClientProvider>
+  );
 }
